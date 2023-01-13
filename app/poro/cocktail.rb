9b412ba106bba342aef62bcca760185fc5be027c
Gauri Joshi <gauri728@gmail.com>
2023-01-13 10:27:38 -0800

    Test and Feature: add measurements of each ingredient on view page

--- app/poro/cocktail.rb
+++ app/poro/cocktail.rb
@@ -2,13 +2,19 @@ class Cocktail
   attr_reader :name, :ingredient1, 
               :ingredient2, :ingredient3, 
               :ingredient4, :instructions,
-              :image 
+              :image, :measurement1, :measurement2,
+              :measurement3
   def initialize(data)
     @name = data[:drinks][0][:strDrink]
     @ingredient1 = data[:drinks][0][:strIngredient1]
     @ingredient2 = data[:drinks][0][:strIngredient2]
     @ingredient3 = data[:drinks][0][:strIngredient3]
     @ingredient4 = data[:drinks][0][:strIngredient4]
+    @measurement1 = data[:drinks][0][:strMeasure1]
+    @measurement2 = data[:drinks][0][:strMeasure2]
+    @measurement3 = data[:drinks][0][:strMeasure3]
+    # measurements for each ingredient
+    # require 'pry'; binding.pry 
     @instructions = data[:drinks][0][:strInstructions]
     @image = data[:drinks][0][:strDrinkThumb]
   end
--- spec/features/user_can_search_by_liquor_ingredient_spec.rb
+++ spec/features/user_can_search_by_liquor_ingredient_spec.rb
@@ -11,6 +11,11 @@ RSpec.describe 'as a visitor' do
       click_button 'Show me some cocktails!'
 
       expect(current_path).to eq(drinks_path)
+      expect(page).to have_content("Here Are Some Recommendations:")
     end
   end
 end
+
+# I want to add a feature that displays these cocktails as links to that cocktail's show page
+# with the name, ingredients, instructions
+# and a home button to go back to the landing page on the show pages
